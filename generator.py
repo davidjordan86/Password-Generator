e7b70d400b89da4f14ee2b90971923ff4d41a892
David <davidjordan86@gmail.com>
2020-09-27 10:29:33 -0500

    finished main and fixed checks

--- generator.py
+++ generator.py
@@ -1,14 +1,14 @@
-import sys
-import string
 
 # used to get the website name and return it
 def get_website_name():
     website_name = input("What website are you creating a password for? ")
     return website_name
 
-#used to get the base phrase for the password and return it
+
+# used to get the base phrase for the password and return it
 def get_base_phrase():
-    base_phrase = input('''Pick a word or phrase that your password will be based on (do not include spaces or special characters): ''')
+    base_phrase = input("Pick a word or phrase that your password will be based on \
+(do not include spaces or special characters): ")
     return base_phrase
 
 
@@ -17,31 +17,28 @@ def get_first_three_letters(string):
     return first_three_letters
 
 
-
 def generate_password():
     print("function")
 
 
 def main():
-    #get the website name from user
+    # get the website name from user
     website = get_website_name()
     
-    #Check if it is valid and keep looping until they provide a valid name
-    while not website.isalpha() and website.lenth() > 3:
+    # Check if it is valid and keep looping until they provide a valid name
+    while len(website) < 3 or not website.isalpha():
         website = get_website_name()
 
     phrase = get_base_phrase()
-    password = ""
 
     while not phrase.isalpha():
         phrase = get_base_phrase()
     
     letters = get_first_three_letters(website).capitalize()
 
-    password = letters + "!" + phrase.lower() +
+    password = letters + "!" + phrase.lower() + str(len(website))
 
-    
-
+    print(password)
 
 
 if __name__ == "__main__":
